8071a559dbc084c0423ed54c5b2959ab972e6665
An0nym0usShad0w <76418474+An0nym0usShad0w@users.noreply.github.com>
2025-02-12 13:52:08 +0600

    updated react route

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
         <NavBar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/react_movie" element={<Home />} />
+            <Route path="/react_movie/favorites" element={<Favorites />} />
           </Routes>
         </main>
       </MovieProvider>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,10 @@ import "../css/Navbar.css"
 function NavBar(){
      return <nav className = "navbar">
           <div className ="navbar-brand">
-               <Link to="/">Movie App</Link>
+               <Link to="/react_movie">Movie App</Link>
           </div>
           <div className="navbar-links">
-               <Link to = "/favorites" className="nav-link">Favorites</Link>
+               <Link to = "/react_movie/favorites" className="nav-link">Favorites</Link>
           </div>
      </nav>
 }
